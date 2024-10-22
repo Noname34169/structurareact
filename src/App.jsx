06b72df8a8c6b405bf848e47d593d6e1d6a03fe4
Noname34169 <salomovasil0@gmail.com>
2024-10-22 16:56:51 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Section from './components/Section/Section'
+import About from './components/About/About'
+import Button from './components/Button/Button'
+import Card from './components/Card/Card'
+import Hero from './components/Hero/Hero'
+import Wrapper from './components/Wrapper/Wrapper'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    <Section/>
+    <About/>
+    <Button/>
+    <Card/>
+    <Hero/>
+    <Wrapper/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Wrapper.css'
+
+const Wrapper = () => {
+  return (
+    <>
+    <h2>Wrapper</h2>
+    </>
+  )
+}
+
+export default Wrapper
